0280442176a7d3bd9359ededf8ca2724234f66b4
KanuriSathvika <kanurisathvika310.com>
2024-08-26 21:12:03 +0530

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-react-deploy/",
+  base: "/Food-Delivery-Ecommerce-Website/", // Set this to your repository name
 })
